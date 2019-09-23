ef8b1af3f7d12f27c0960be03811a7c8f22e5612
cocpizano0@outlook.com <cocpizano0@outlook.com>
2019-09-23 18:55:44 -0500

    Inicio del proyecto

---
+++ PruebaNetFramework/PruebaNetFramework/Data/CatalogosDbContext.cs
@@ -0,0 +1,25 @@
+﻿using PruebaNetFramework.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PruebaNetFramework.Data
+{
+    public class CatalogosDbContext : DbContext
+    {
+        public CatalogosDbContext() :
+         base("BDCatalogosEntities")
+        {
+        }
+
+        public static InventarioDbContext Create()
+        {
+            return new InventarioDbContext();
+        }
+
+        public DbSet<SubCategoria> SubCategoria { get; set; }
+        public DbSet<Categoria> Categoria { get; set; }
+    }
+}
---
+++ PruebaNetFramework/PruebaNetFramework/EntityModels/Articulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PruebaNetFramework.EntityModels
+{
+    public class Articulo
+    {
+        [Key]
+        public int Sku_ID { get; set; }
+        public string Sku_NumeroSerie { get; set; }
+        public string Sku_Descripcion { get; set; }
+        public string Sku_Cantidad { get; set; }
+        [ForeignKey("Categoria")]
+        public int Sku_Cat_ID { get; set; }
+        [ForeignKey("SubCategoria")]
+        public int Sku_Sub_Cat_ID { get; set; }
+        public string Sku_Latitud { get; set; }
+        public string Sku_Longitud { get; set; }
+
+        public virtual Categoria Categoria { get; set; }
+        public virtual SubCategoria SubCategoria { get; set; }
+    }
+}
